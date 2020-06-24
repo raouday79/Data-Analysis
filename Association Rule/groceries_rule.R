dd5397da89a39d251ecc6bb00cfd81eb126574c8
raouday79 <31645574+raouday79@users.noreply.github.com>
2020-06-25 00:53:41 +0530

    Update groceries_rule.R

--- Association Rule/groceries_rule.R
+++ Association Rule/groceries_rule.R
@@ -10,12 +10,33 @@ library(arules)
 dataset = read.transactions(file.choose(),sep=",",rm.duplicates = TRUE)
 View(dataset)
 summary(dataset)
+
+library(arulesViz)
+
 #Plot showing the most buy items
 itemFrequencyPlot(x=dataset,topN=15)
 
 #Building Rule
-#Support of 3 items buy in day
+#Support of 3 items buy in day... rule for the most frequent but items
 rule = apriori(data=dataset,parameter = list(support=0.003,confidence=0.4))
 
 #Inspecting the rules
 inspect(sort(rule,by="lift")[1:10])
+
+
+#Rule for less frequent item in a day
+
+rule2 = apriori(data=dataset,parameter = list(support=0.01,confidence=0.3))
+
+#Inspecting the rules
+inspect(sort(rule2,by="lift")[1:10])
+
+plot(rule2)  # less support has high lift value and low confidence
+
+
+#plotting the bubble chart
+plot(rule2,method="grouped")
+
+#graph plot of rule
+plot(rule2,method="graph",max=10)
+
