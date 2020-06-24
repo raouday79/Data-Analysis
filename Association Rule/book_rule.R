2983c9a002c99b9b704011c0311e16e7f54f96e1
raouday79 <31645574+raouday79@users.noreply.github.com>
2020-06-25 00:52:57 +0530

    Update book_rule.R

--- Association Rule/book_rule.R
+++ Association Rule/book_rule.R
@@ -11,7 +11,7 @@ str(data)
 #We need data in sparse matrix form
 #Read the data set as transection.
 #Using the arules library
-#install.packages("arules")
+install.packages("arules")
 library(arules)
 trans = as(data,"transactions")
 
@@ -20,7 +20,8 @@ summary(trans)
 
 
 #Plot showing the most buy items
-itemFrequencyPlot(x=trans,topN=15)
+itemFrequencyPlot(x=trans,topN=10)
+#from the above plot most frequenct bookbought was ItalAtlas, ItalArt, Florence, ItalCook
 
 #Building Rule
 #Support of 3 items buy in day
@@ -28,4 +29,33 @@ rule = apriori(data=trans,parameter = list(support=0.04,confidence=0.4))
 
 #Inspecting the rules
 inspect(sort(rule,by="lift")[1:10])
+
+# Genearating the moore stronger rule by increasing the confidence value.
+
+rul2 = apriori(data=trans,parameter = list(support=0.2,confidence=0.5))
+inspect(sort(rul2,by="lift")[1:10])
+
+
+# ule for the more frequent buy books...
+rule3 = apriori(data=trans,parameter = list(support=0.5,confidence=0.6))
+inspect(sort(rule3,by="lift")[1:10])
+
+#install.packages("aruleViz")
+library(arulesViz)
+plot(rul2)
+#From the above plot we can anlyze there is high lift for low support value. for the confidence value between 0.7 to 0.8
+
+dia1 = plot(rul2,measure = c("support","lift"),shading = "confidence",interactive = TRUE)
+
+arules::inspect(rul2)
+#plotting the bubble chart
+plot(rul2,method="grouped")
+
+#graph plot of rule
+plot(rul2,method="graph",max=20)
+
+
+
+
+
 
