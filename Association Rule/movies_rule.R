380300f6138715019ca34caeefaf769552268dcc
raouday79 <31645574+raouday79@users.noreply.github.com>
2020-06-25 00:54:19 +0530

    Update movies_rule.R

--- Association Rule/movies_rule.R
+++ Association Rule/movies_rule.R
@@ -16,6 +16,7 @@ data[c(1:10)] = lapply(data[c(1:10)], factor)
 #Using the arules library
 #install.packages("arules")
 library(arules)
+library(arulesViz)
 trans = as(data,"transactions")
 #dataset = read.transactions(name,cols =2 , sep=",",rm.duplicates = TRUE)
 View(trans)
@@ -24,7 +25,23 @@ summary(trans)
 itemFrequencyPlot(x=trans,topN=10)
 #Building Rule
 #Support of 2
-rule = apriori(data=dataset,parameter = list(support=0.02,confidence=0.3))
+rule = apriori(data=trans,parameter = list(support=0.02,confidence=0.3))
 
 #Inspecting the rules
 inspect(sort(rule,by="lift")[1:4])
+plot(rule)
+
+#Support of 5
+rule2 = apriori(data=trans,parameter = list(support=0.5,confidence=0.4))
+#23550 rules
+#Inspecting the rules
+inspect(sort(rule2,by="lift")[1:4])
+plot(rule2)
+#graph plot of rule
+plot(rule2,method="graph",max=10)
+#people who watch harry potter 1  also watch harry potter 2 vice versa is also true from the above rule.
+
+
+
+
+
